7715f0917afe1f70b8b6cc24a4048ab034b13e48
jaexxin <jmsb02@naver.com>
2025-01-23 12:23:48 +0900

    [chore] 내부 파일 정리

--- src/main/java/com/backend/web/controller/AuthController.java
+++ src/main/java/com/backend/web/controller/AuthController.java
@@ -20,26 +20,8 @@ public class AuthController {
 
     private final AuthService authService; // 의존성 주입
 
-    @GetMapping("/protected")
-    public String getProtectedResource() {
-        // 인증된 사용자만 접근 가능
-        return "접근 성공";
-    }
-
-    @GetMapping("/")
-    public String getFailResource() {
-        // 인증된 사용자만 접근 가능
-        return "접근 실패";
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> join(@RequestBody UserRequestDTO.LoginRequestDTO loginRequestDTO) {
-        return null;
-    }
-
     @GetMapping("login/oauth2/code/kakao")
     public BaseResponse<UserResponseDTO.JoinResultDTO> kakaoLogin(@RequestParam("code") String accessCode, HttpServletResponse httpServletResponse) {
-        log.info("Received Kakao authorization code: {}", accessCode);
         User user = authService.oAuthLogin(accessCode, httpServletResponse);
         return BaseResponse.onSuccess(AuthConverter.toJoinResultDTO(user));
     }
